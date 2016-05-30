136c661510ea46f0680677fc3165e6d10e6674a1
Miguel Peniche <miguel@bengala.mx>
2016-05-30 11:26:56 -0500

    Add first styles

--- shared/components/footprint/_footprint.rt.js
+++ shared/components/footprint/_footprint.rt.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
 export default function () {
-    return React.createElement('div', {
-        'className': 'cc-component',
+    return React.createElement('main', {
+        'className': 'cc-component container container-sm',
         'id': 'footprint'
-    }, React.createElement('div', { 'className': 'cc-component__header' }, React.createElement('img', { 'src': '/assets/img/' + this.route_key + '.png' }), React.createElement('span', {}, this.title)), React.createElement('div', { 'className': 'cc-component__form' }), React.createElement('div', { 'className': 'cc-component__nav' }, React.createElement('button', {
-        'onClick': this.router.previous.bind(this.router),
-        'className': 'btn'
-    }, this.t('Previous')), React.createElement('button', {
-        'onClick': this.router.next.bind(this.router),
-        'className': 'btn'
-    }, this.t('Next'))));
+    }, React.createElement('header', { 'className': 'cc-component__header' }, React.createElement('img', { 'src': '/assets/img/' + this.route_key + '.png' }), React.createElement('span', {}, this.title)), React.createElement('div', { 'className': 'cc-component__form' }), React.createElement('div', { 'className': 'cc-component__nav' }, React.createElement('a', { 'onClick': this.router.previous.bind(this.router) }, this.t('Previous')), React.createElement('a', { 'onClick': this.router.next.bind(this.router) }, this.t('Next'))));
 }
--- shared/components/layout/_layout.rt.js
+++ shared/components/layout/_layout.rt.js
@@ -10,7 +10,7 @@ import TakeAction from './../take_action/take_action.component';
 import Missing from './../missing/missing.component';
 import Graphs from './../graphs/graphs.component';
 function repeatRoute1(route, routeIndex) {
-    return React.createElement('div', {
+    return React.createElement('li', {
         'className': 'cc-nav__item' + ' ' + _({ 'cc-nav__item--active': this.current_route_name === route.route_name }).transform(function (res, value, key) {
             if (value) {
                 res.push(key);
@@ -21,9 +21,12 @@ function repeatRoute1(route, routeIndex) {
     }, React.createElement('img', { 'src': '/assets/img/' + route.key + '.png' }), React.createElement('span', {}, this.t(`${ route.key }.title`)));
 }
 export default function () {
-    return React.createElement('div', { 'id': 'cc_calculator_ui' }, React.createElement.apply(this, [
-        'div',
-        { 'id': 'cc_nav' },
+    return React.createElement('div', { 'id': 'cc_calculator_ui' }, React.createElement('nav', {
+        'id': 'cc_nav',
+        'className': 'navbar navbar-default'
+    }, React.createElement('div', { 'className': 'navbar-collapse collapse' }, React.createElement.apply(this, [
+        'ul',
+        { 'className': 'nav navbar-nav' },
         _.map(this.router.main_routes, repeatRoute1.bind(this))
-    ]), React.createElement('div', { 'id': 'banner' }), React.createElement('div', {}, this.current_route_name === 'GetStarted' ? React.createElement(GetStarted, {}) : null, this.current_route_name === 'Travel' ? React.createElement(Travel, {}) : null, this.current_route_name === 'Home' ? React.createElement(Home, {}) : null, this.current_route_name === 'Food' ? React.createElement(Food, {}) : null, this.current_route_name === 'Shopping' ? React.createElement(Shopping, {}) : null, this.current_route_name === 'Footprint' ? React.createElement(Footprint, {}) : null, this.current_route_name === 'TakeAction' ? React.createElement(TakeAction, {}) : null, this.current_route_name === 'Missing' ? React.createElement(Missing, {}) : null), React.createElement(Graphs, {}));
+    ]))), React.createElement('div', { 'id': 'banner' }), React.createElement('main', {}, this.current_route_name === 'GetStarted' ? React.createElement(GetStarted, {}) : null, this.current_route_name === 'Travel' ? React.createElement(Travel, {}) : null, this.current_route_name === 'Home' ? React.createElement(Home, {}) : null, this.current_route_name === 'Food' ? React.createElement(Food, {}) : null, this.current_route_name === 'Shopping' ? React.createElement(Shopping, {}) : null, this.current_route_name === 'Footprint' ? React.createElement(Footprint, {}) : null, this.current_route_name === 'TakeAction' ? React.createElement(TakeAction, {}) : null, this.current_route_name === 'Missing' ? React.createElement(Missing, {}) : null), React.createElement(Graphs, {}));
 }
