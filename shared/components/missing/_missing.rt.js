b5bb84fe0cb4c682aa50515f7f9d123584387cc3
Eric Hulburd <eric@arbol.org>
2016-06-28 00:19:08 -0500

    git ignore

--- shared/components/missing/_missing.rt.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import _ from 'lodash';
-export default function () {
-    return React.createElement('div', { 'className': 'alert alert-danger' }, this.t('route_doesnt_exist'));
-}
