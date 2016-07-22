ea842b29331a9518b082887ef52931335c29208b
manue1 <manuel.bergler@gmail.com>
2016-07-22 17:14:34 -0500

    Handle empty list of leaders, stop retrieving when all data is loaded.

--- shared/components/leaders/leaders.component.js
+++ shared/components/leaders/leaders.component.js
@@ -18,7 +18,8 @@ class LeadersComponent extends Panel {
       cache: [],
       total_count: 0,
       trigger_update: true,
-      is_loading: false
+      is_loading: false,
+      all_loaded: false
     }
   }
 
@@ -31,7 +32,11 @@ class LeadersComponent extends Panel {
   }
 
   get total_count_reached(){
-    return (this.state.limit + this.state.offset) >= this.state.total_count;
+    if (this.state.offset === 0 && this.state.offset === 20) {
+      return this.state.limit >= this.state.total_count
+    } else {
+      return this.state.offset + this.state.limit >= this.state.total_count;
+    }
   }
 
   componentDidMount() {
@@ -60,15 +65,14 @@ class LeadersComponent extends Panel {
   detectScroll() {
     let leaders = this;
     $(window).scroll(function(){
-      let window_top = $(window).scrollTop(), doc_height = $(document).height(), window_height = $(window).height();
+      let window_top = $(window).scrollTop(), doc_height = $(document).height(), window_height = $(window).height(), scrolltrigger = 0.95;
 
       leaders.setState({
         scrolled: (window_top/(doc_height-window_height))*100,
       })
 
-      if (leaders.state.scrolled >= 75 && leaders.state.trigger_update) {
+      if (leaders.state.scrolled >= 75 && leaders.state.trigger_update && !leaders.total_count_reached && !leaders.state.all_loaded) {
         leaders.setState({
-          limit: leaders.state.limit + 20,
           offset: leaders.state.offset + 20,
           is_loading: true,
           trigger_update: false
@@ -77,6 +81,9 @@ class LeadersComponent extends Panel {
         setTimeout(function() {
           leaders.retrieveLeaders().then(() => {
             leaders.showRetrievedLeaders();
+            leaders.setState({
+              trigger_update: true
+            })
           });
         }, 1000);
       }
@@ -88,16 +95,21 @@ class LeadersComponent extends Panel {
     return new Promise((resolve, reject) => {
       listLeaders(leaders.state.limit, leaders.state.offset).then((res) => {
         if (res.success) {
-          leaders.setState({
-            cache: leaders.state.cache.concat(res.data.list),
-            total_count: res.data.total_count
-          });
+          if (res.data.list != null) {
+            leaders.setState({
+              cache: leaders.state.cache.concat(res.data.list),
+              total_count: res.data.total_count
+            });
+          } else {
+            leaders.setState({
+              all_loaded: true
+            });
+          }
           if (res.data.total_count > 0) {
             resolve();
           } else {
             reject("total_count=0");
           }
-
         } else {
           leaders.state_manager.state.alerts.push({type: 'danger', message: leaders.t('leaders.retrieval_error')});
           leaders.state_manager.syncLayout();
