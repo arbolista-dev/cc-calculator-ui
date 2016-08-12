7854edd72f031092fee37e7490b5f413827eb5db
manue1 <manuel.bergler@gmail.com>
2016-08-12 17:47:15 -0500

    Make sure all form input keys are populated.

--- shared/components/take_action/action/action.component.js
+++ shared/components/take_action/action/action.component.js
@@ -102,7 +102,11 @@ class ActionComponent extends Translatable {
       id = id.replace(/display_takeaction/i, 'input_takeaction')
     }
     console.log(id, '= ', this.state_manager.state.user_footprint[id]);
-    return this.state_manager.state.user_footprint[id] + ' ' + suffix
+    if (!suffix) {
+      return this.state_manager.state.user_footprint[id]
+    } else {
+      return this.state_manager.state.user_footprint[id] + ' ' + suffix
+    }
   }
 
   updateActionInput(event){
@@ -139,49 +143,39 @@ class ActionComponent extends Translatable {
   handleChange(event){
     let i = event.target.value,
     is_vehicle = event.target.id.lastIndexOf('vehicle_select'),
-    action_key = event.target.dataset.action_key;
+    action_key = event.target.dataset.action_key,
+    id = event.target.id;
 
     console.log('handleChange action key: ', action_key)
     console.log('handleChange value: ', i)
-    console.log('handleChange id: ', event.target.id)
+    console.log('handleChange id: ', id)
 
-    // @ToDo: updateTakeaction first and then show results --> Air travel needs updated miles_alt state!
-
-    if (is_vehicle > 0) this.selectVehicle(i, action_key)
-
-    if (action_key === 'reduce_air_travel') {
-
-      // console.log("--- miles_percent BEFORE", footprint['input_takeaction_reduce_air_travel_miles_percent'])
-
+    if (is_vehicle > 0) {
+      this.selectVehicle(i, action_key)
+    } else {
       let update = {};
-      update['input_takeaction_reduce_air_travel_miles_percent'] = i;
+      update[id] = i;
       this.setState(update);
       this.updateTakeaction(update);
-      // this.state_manager.syncLayout()
-      // footprint['input_takeaction_reduce_air_travel_miles_percent'] = i;
-
-      // let footprint = this.state_manager.state.user_footprint;
-      // console.log("--- miles_percent", footprint['input_takeaction_reduce_air_travel_miles_percent'])
-      //
-      // console.log("footprint['result_takeaction_reduce_air_travel_miles_alt']", footprint['result_takeaction_reduce_air_travel_miles_alt'])
-      // $('#result_takeaction_reduce_air_travel_miles_alt').text(footprint['result_takeaction_reduce_air_travel_miles_alt']).append(' fewer miles per year.');
     }
+
+    // if (action_key === 'reduce_air_travel') {
+    //   let update = {};
+    //   update['input_takeaction_reduce_air_travel_miles_percent'] = i;
+    //   this.setState(update);
+    //   this.updateTakeaction(update);
+    // }
 
   }
 
   selectVehicle(i, action_key){
-    // on update, call setState and updateTakeaction
 
     let footprint = this.state_manager.state.user_footprint,
     v_miles = footprint[`input_footprint_transportation_miles${i}`],
     v_mpg = footprint[`input_footprint_transportation_mpg${i}`],
     update = {};
 
-    console.log('vehicle miles', v_miles);
-
-
     if (action_key === 'ride_my_bike' || action_key ===  'telecommute_to_work' || action_key ===  'take_public_transportation') {
-      console.log("('ride_my_bike' || 'telecommute_to_work' || 'take_public_transportation'): ", action_key);
 
       update['input_takeaction_' + action_key + '_mpg'] = parseInt(v_mpg);
       this.setState(update);
@@ -197,43 +191,53 @@ class ActionComponent extends Translatable {
 
   }
 
-  calcVehicleTotal(action_key){
+  // calcVehicleTotal(action_key){
+  //
+  //   let footprint = this.state_manager.state.user_footprint,
+  //       no_vehicles = this.state_manager.state.vehicles.length,
+  //       total_miles = 0,
+  //       total_mpg = 0,
+  //       update = {};
+  //
+  //   this.state_manager.state.vehicles.forEach((v) => {
+  //     total_miles += parseInt(v.miles);
+  //     total_mpg += parseInt(v.mpg);
+  //   });
+  //
+  //   if (action_key === 'practice_eco_driving') {
+  //     let newmpg = 'result_takeaction_practice_eco_driving_newmpg',
+  //     galsaved = 'result_takeaction_practice_eco_driving_galsaved';
+  //
+  //     // update['result_takeaction_practice_eco_driving_dispmiles'] = total_miles;
+  //     // update[newmpg] = Math.round(footprint[newmpg]);
+  //     // update[galsaved] = Math.round(footprint[galsaved]);
+  //     // update['result_takeaction_practice_eco_driving_mpg'] = total_mpg / no_vehicles;
+  //     // this.setState(update);
+  //     // this.updateTakeaction(update);
+  //
+  //     // footprint['result_takeaction_practice_eco_driving_dispmiles'] = total_miles;
+  //     // $('#result_takeaction_practice_eco_driving_dispmiles').text(total_miles).append(' ' + this.t(`travel.miles_abbr`));
+  //     //
+  //     // footprint['result_takeaction_practice_eco_driving_mpg'] = total_mpg / no_vehicles;
+  //     // $('#result_takeaction_practice_eco_driving_mpg').text(total_mpg / no_vehicles).append(' ' + this.t(`travel.miles_per_gallon`));
+  //     //
+  //     //
+  //     // $('#' + newmpg).text(Math.round(footprint[newmpg])).append(' ' + this.t(`travel.miles_per_gallon`));
+  //     // $('#' + galsaved).text(Math.round(footprint[galsaved])).append(' ' + this.t(`travel.gallons_per_year`));
+  //
+  //   } else {
+  //     $('#result_takeaction_maintain_my_vehicles_dispmiles').text(total_miles).append(' ' + this.t(`travel.miles_abbr`));
+  //     $('#result_takeaction_maintain_my_vehicles_mpg').text(total_mpg / no_vehicles).append(' ' + this.t(`travel.miles_per_gallon`));
+  //
+  //   }
+  // }
 
-    let footprint = this.state_manager.state.user_footprint,
-        no_vehicles = this.state_manager.state.vehicles.length,
-        total_miles = 0,
-        total_mpg = 0;
-
-    this.state_manager.state.vehicles.forEach((v) => {
-      total_miles += parseInt(v.miles);
-      total_mpg += parseInt(v.mpg);
-    });
-
-    if (action_key === 'practice_eco_driving') {
-      footprint['result_takeaction_practice_eco_driving_dispmiles'] = total_miles;
-      $('#result_takeaction_practice_eco_driving_dispmiles').text(total_miles).append(' ' + this.t(`travel.miles_abbr`));
-      footprint['result_takeaction_practice_eco_driving_mpg'] = total_mpg / no_vehicles;
-      $('#result_takeaction_practice_eco_driving_mpg').text(total_mpg / no_vehicles).append(' ' + this.t(`travel.miles_per_gallon`));
-
-      let newmpg = 'result_takeaction_practice_eco_driving_newmpg',
-          galsaved = 'result_takeaction_practice_eco_driving_galsaved';
-
-      $('#' + newmpg).text(Math.round(footprint[newmpg])).append(' ' + this.t(`travel.miles_per_gallon`));
-      $('#' + galsaved).text(Math.round(footprint[galsaved])).append(' ' + this.t(`travel.gallons_per_year`));
-
-    } else {
-      $('#result_takeaction_maintain_my_vehicles_dispmiles').text(total_miles).append(' ' + this.t(`travel.miles_abbr`));
-      $('#result_takeaction_maintain_my_vehicles_mpg').text(total_mpg / no_vehicles).append(' ' + this.t(`travel.miles_per_gallon`));
-
-    }
-  }
-
-  getAirTotal(){
-    let footprint = this.state_manager.state.user_footprint;
-
-    $('#result_takeaction_reduce_air_travel_totalmiles').text(footprint['result_takeaction_reduce_air_travel_totalmiles']).append(' ' + this.t(`travel.miles_per_year`));
-    $('#result_takeaction_reduce_air_travel_pounds_from_flight').text(footprint['result_takeaction_reduce_air_travel_pounds_from_flight']).append(' ' + this.t(`travel.co2_per_year`));
-  }
+  // getAirTotal(){
+  //   let footprint = this.state_manager.state.user_footprint;
+  //
+  //   $('#result_takeaction_reduce_air_travel_totalmiles').text(footprint['result_takeaction_reduce_air_travel_totalmiles']).append(' ' + this.t(`travel.miles_per_year`));
+  //   $('#result_takeaction_reduce_air_travel_pounds_from_flight').text(footprint['result_takeaction_reduce_air_travel_pounds_from_flight']).append(' ' + this.t(`travel.co2_per_year`));
+  // }
 
   userApiValue(api_key){
     return this.state_manager.user_footprint[api_key];
@@ -297,8 +301,8 @@ class ActionComponent extends Translatable {
 
   componentDidMount(){
     if (this.category === 'transportation') this.selectVehicle(1, this.key);
-    if (this.key === 'practice_eco_driving' || this.key === 'maintain_my_vehicles') this.calcVehicleTotal(this.key);
-    if (this.key === 'reduce_air_travel') this.getAirTotal();
+    // if (this.key === 'practice_eco_driving' || this.key === 'maintain_my_vehicles') this.calcVehicleTotal(this.key);
+    // if (this.key === 'reduce_air_travel') this.getAirTotal();
   }
 
   render(){
