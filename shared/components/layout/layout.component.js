b7c7c2c9b78ec728afa96ff132cdce06cc5903a1
Eric Hulburd <eric@arbol.org>
2016-08-26 15:29:18 -0500

    hide leaders with settings

--- shared/components/layout/layout.component.js
+++ shared/components/layout/layout.component.js
@@ -43,7 +43,7 @@ class LayoutComponent extends mixin(Translatable, routable) {
   get show_leaders_comparison(){
     let leaders_route = NON_LEADERS_PANELS.indexOf(this.current_route_name) < 0;
 
-    return leaders_route && this.state_manager.state.leaders_chart.show;
+    return this.show_settings && leaders_route && this.state_manager.state.leaders_chart.show;
   }
 
   get external_offset(){
