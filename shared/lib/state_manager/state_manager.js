22dffb40a2529eee196475ad6c195a09f5cc5078
manue1 <manuel.bergler@gmail.com>
2016-10-12 13:10:12 -0500

    Store UI state in localStorage to keep selected location_mode on page reload.

--- shared/lib/state_manager/state_manager.js
+++ shared/lib/state_manager/state_manager.js
@@ -52,6 +52,10 @@ export default class StateManager {
     return getLocalStorageItem('auth');
   }
 
+  get ui_storage(){
+    return getLocalStorageItem('ui');
+  }
+
   get default_ui_state(){
     let ui_state = {
       external_offset: {},
@@ -116,7 +120,7 @@ export default class StateManager {
         result_takeaction_net10yr: state_manager.take_action_storage.net10yr || {},
         result_takeaction_pounds:  state_manager.take_action_storage.pounds || {}
       }),
-      ui: fromJS(state_manager.default_ui_state)
+      ui: fromJS(state_manager.ui_storage || state_manager.default_ui_state)
     }, opts);
   }
 
--- shared/reducers/ui/ui.reducers.js
+++ shared/reducers/ui/ui.reducers.js
@@ -2,6 +2,8 @@ import { fromJS, Map, List } from 'immutable';
 import { createReducer } from 'redux-act';
 
 import { updateUI, pushAlert, resetAlerts } from './ui.actions'
+import { setLocalStorageItem } from 'shared/lib/utils/utils';
+
 /*
   ui: {
     external_offset: <Map>,
@@ -17,21 +19,25 @@ import { updateUI, pushAlert, resetAlerts } from './ui.actions'
 const ACTIONS = {
 
   [updateUI]: (state, payload)=>{
+    let updated;
     if (state.has(payload.id)) {
       if (Map.isMap(state.get(payload.id))) {
-        return fromJS(state.set(payload.id, state.get(payload.id).merge(payload.data)));
+        updated = fromJS(state.set(payload.id, state.get(payload.id).merge(payload.data)));
       } else {
-        return fromJS(state.set(payload.id, payload.data));
+        updated = fromJS(state.set(payload.id, payload.data));
       }
     } else {
-      return fromJS(state.set(payload.id, payload.data));
+      updated = fromJS(state.set(payload.id, payload.data));
     }
+    setLocalStorageItem('ui', updated.toJS());
+    return updated;
   },
 
   [pushAlert]: (state, payload)=>{
     state = state.setIn(['alerts', payload.id], new List(payload.data))
                  .set('alert_exists', true);
 
+    setLocalStorageItem('ui', state.toJS());
     return state
   },
 
@@ -41,6 +47,7 @@ const ACTIONS = {
     })
     state = state.set('alert_exists', false);
 
+    setLocalStorageItem('ui', state.toJS());
     return state
   }
 
