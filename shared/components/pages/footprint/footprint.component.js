7dde9a72354d5dea77d498db02bfe2ed60065a46
manue1 <manuel.bergler@gmail.com>
2017-02-07 13:32:53 -0600

    Add goToProfile from footprint component.

--- shared/components/pages/footprint/footprint.component.js
+++ shared/components/pages/footprint/footprint.component.js
@@ -7,19 +7,27 @@ import template from './footprint.rt.html';
 
 class FootprintComponent extends Panel {
 
-  constructor(props, context) {
-    super(props, context);
-    const footprint = this;
-    footprint.state = {};
-  }
+    constructor(props, context) {
+        super(props, context);
+        const footprint = this;
+        footprint.state = {};
+    }
 
-  get show_leaders_comparison() {
-    return this.connect_to_api && this.props.ui.get('show_leaders_chart');
-  }
+    get show_leaders_comparison() {
+        return this.connect_to_api && this.props.ui.get('show_leaders_chart');
+    }
 
-  render() {
-    return template.call(this);
-  }
+    get user_id() {
+        return this.props.auth.getIn(['data', 'user_id']);
+    }
+
+    viewProfile() {
+        this.goToProfile(this.user_id);
+    }
+
+    render() {
+        return template.call(this);
+    }
 }
 
 FootprintComponent.propTypes = footprintPropTypes;
