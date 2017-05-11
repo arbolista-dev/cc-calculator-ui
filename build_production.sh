90ba67bf52a9e7bb0dc891dd83baf6e21622efbd
manue1 <manuel.bergler@gmail.com>
2017-05-11 11:45:16 -0500

    Fix build script and setup Firebase.

--- build_production.sh
+++ build_production.sh
@@ -1,6 +1,8 @@
-rm -r ./build
+rm -rf ./build
 mkdir -p ./build/production/assets/font-awesome
 cp -rf ./server/assets/* ./build/production/assets/
 cp -rf ./node_modules/font-awesome/ ./build/production/assets/font-awesome/
 mv ./build/production/assets/favicon.ico ./build/production/favicon.ico
 npm run build_production
+cp -f ./server/views/index.ejs ./build/production/index.html
+sed -i '32,33d' ./build/production/index.html
