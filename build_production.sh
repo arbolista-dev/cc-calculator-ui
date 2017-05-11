050ad98bc41ab8b866562a2511499e13c4451135
manue1 <manuel.bergler@gmail.com>
2017-05-11 12:06:49 -0500

    Fix font awesome path in build.

--- build_production.sh
+++ build_production.sh
@@ -1,7 +1,7 @@
 rm -rf ./build
 mkdir -p ./build/production/assets/font-awesome
 cp -rf ./server/assets/* ./build/production/assets/
-cp -rf ./node_modules/font-awesome/ ./build/production/assets/font-awesome/
+cp -rf ./node_modules/font-awesome/ ./build/production/assets/
 mv ./build/production/assets/favicon.ico ./build/production/favicon.ico
 npm run build_production
 cp -f ./server/views/index.ejs ./build/production/index.html
