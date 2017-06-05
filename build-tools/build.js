0c489baef486ea196047a89b2f5a8f6aa2ff0788
manue1 <manuel.bergler@gmail.com>
2017-06-05 18:48:45 -0500

    Setup Firebase for production build.

--- build-tools/build.js
+++ build-tools/build.js
@@ -9,20 +9,20 @@ process.env.NODE_ENV = argv.env || 'development';
 process.env.APP_ID = '651280398386350';
 
 if (argv.local_api) {
-  process.env.API_BASE_URL = 'http://localhost:8082';
+    process.env.API_BASE_URL = 'http://localhost:8082';
 } else {
-  // This assumes a proxy intercept requests and divert to cc-user-api app.
-  process.env.API_BASE_URL = 'http://calculator.coolclimatenetwork.net';
+    // This assumes a proxy intercept requests and divert to cc-user-api app.
+    process.env.API_BASE_URL = 'https://calculator.coolclimatenetwork.net';
 }
 
 export default function build(options, done) {
-  // build assets/app.js and assets/style.css with webpack
-  const config = require(`${__dirname}/../client/config/webpack/${process.env.NODE_ENV}`);
-  webpack(config, (err) => {
-    if (err) {
-      console.error('=== Error building webpack config ===');
-      console.error(err);
-    }
-    return done();
-  });
+    // build assets/app.js and assets/style.css with webpack
+    const config = require(`${__dirname}/../client/config/webpack/${process.env.NODE_ENV}`);
+    webpack(config, (err) => {
+        if (err) {
+            console.error('=== Error building webpack config ===');
+            console.error(err);
+        }
+        return done();
+    });
 }
--- client/api/real/calculator.api.js
+++ client/api/real/calculator.api.js
@@ -12,93 +12,93 @@ const KEY_ID = '15a335b6';
 const KEY = '26ffc87cf832d3d2c6dfb6241a88937a';
 */
 
-const BASE = 'http://coolclimate.berkeley.edu/calculators/household/api.php';
+const BASE = 'https://coolclimate.berkeley.edu/calculators/household/api.php';
 const APP_TOKEN = 'DPP6zZDkSQAlRKfQ-iWgdg';
 
 class CalculatorApi {
 
-  static computeFootprint(inputs) {
-    return new Promise((fnResolve, fnReject) => {
-      const params = { op: 'compute_footprint' };
-      const payload = Object.assign({}, inputs, {
-        internal_state_abbreviation: 'CA',
-      });
-      superagent.post(BASE)
-        .set('X-DEV-TOKEN', APP_TOKEN)
-        .set('Accept', 'application/x-www-form-urlencoded; charset=UTF-8')
-        .query(params)
-        .send(jQuery.param(payload))
-        .end((err, res) => {
-          if (err) fnReject(err);
-          else {
-            const xml = new DOMParser().parseFromString(res.text, 'application/xml');
-            const parsed = xmlToJson(xml);
-            fnResolve(parsed.response);
-          }
+    static computeFootprint(inputs) {
+        return new Promise((fnResolve, fnReject) => {
+            const params = { op: 'compute_footprint' };
+            const payload = Object.assign({}, inputs, {
+                internal_state_abbreviation: 'CA',
+            });
+            superagent.post(BASE)
+                .set('X-DEV-TOKEN', APP_TOKEN)
+                .set('Accept', 'application/x-www-form-urlencoded; charset=UTF-8')
+                .query(params)
+                .send(jQuery.param(payload))
+                .end((err, res) => {
+                    if (err) fnReject(err);
+                    else {
+                        const xml = new DOMParser().parseFromString(res.text, 'application/xml');
+                        const parsed = xmlToJson(xml);
+                        fnResolve(parsed.response);
+                    }
+                });
         });
-    });
-  }
+    }
 
-  static computeTakeactionResults(footprint) {
-    return new Promise((fnResolve, fnReject) => {
-      const params = { op: 'compute_takeaction_results' };
-      superagent.post(BASE)
-        .set('X-DEV-TOKEN', APP_TOKEN)
-        .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-        .set('Accept', 'application/xml')
-        .query(params)
-        .send(jQuery.param(footprint))
-        .end((err, res) => {
-          if (err) fnReject(err);
-          else {
-            const xml = new DOMParser().parseFromString(res.text, 'application/xml');
-            const parsed = xmlToJson(xml);
-            fnResolve(parsed.response);
-          }
+    static computeTakeactionResults(footprint) {
+        return new Promise((fnResolve, fnReject) => {
+            const params = { op: 'compute_takeaction_results' };
+            superagent.post(BASE)
+                .set('X-DEV-TOKEN', APP_TOKEN)
+                .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+                .set('Accept', 'application/xml')
+                .query(params)
+                .send(jQuery.param(footprint))
+                .end((err, res) => {
+                    if (err) fnReject(err);
+                    else {
+                        const xml = new DOMParser().parseFromString(res.text, 'application/xml');
+                        const parsed = xmlToJson(xml);
+                        fnResolve(parsed.response);
+                    }
+                });
         });
-    });
-  }
+    }
 
-  // eg: location = {input_location: 06704, input_location_mode: 1, input_income: 1, input_size: 0}
-  static getDefaultsAndResults(location) {
-    const params = Object.assign({
-      op: 'get_defaults_and_results',
-    }, location);
-    return new Promise((fnResolve, fnReject) => {
-      superagent.get(BASE)
-        .set('X-DEV-TOKEN', APP_TOKEN)
-        .set('Accept', 'application/xml')
-        .query(params)
-        .end((err, res) => {
-          if (err) fnReject(err);
-          else {
-            const xml = new DOMParser().parseFromString(res.text, 'application/xml');
-            const parsed = xmlToJson(xml);
-            fnResolve(parsed.response);
-          }
+    // eg: location = {input_location: 06704, input_location_mode: 1, input_income: 1, input_size: 0}
+    static getDefaultsAndResults(location) {
+        const params = Object.assign({
+            op: 'get_defaults_and_results',
+        }, location);
+        return new Promise((fnResolve, fnReject) => {
+            superagent.get(BASE)
+                .set('X-DEV-TOKEN', APP_TOKEN)
+                .set('Accept', 'application/xml')
+                .query(params)
+                .end((err, res) => {
+                    if (err) fnReject(err);
+                    else {
+                        const xml = new DOMParser().parseFromString(res.text, 'application/xml');
+                        const parsed = xmlToJson(xml);
+                        fnResolve(parsed.response);
+                    }
+                });
         });
-    });
-  }
+    }
 
-  // eg: location = {input_location: 06704, input_location_mode: 1}
-  static getAutoComplete(location) {
-    const params = Object.assign({
-      op: 'get_auto_complete',
-    }, location);
-    return new Promise((fnResolve, fnReject) => {
-      superagent.get(BASE)
-        .set('X-DEV-TOKEN', APP_TOKEN)
-        .set('Accept', 'application/json')
-        .query(params)
-        .end((err, res) => {
-          if (err) fnReject(err);
-          else {
-            const parsed = JSON.parse(res.text);
-            fnResolve(parsed);
-          }
+    // eg: location = {input_location: 06704, input_location_mode: 1}
+    static getAutoComplete(location) {
+        const params = Object.assign({
+            op: 'get_auto_complete',
+        }, location);
+        return new Promise((fnResolve, fnReject) => {
+            superagent.get(BASE)
+                .set('X-DEV-TOKEN', APP_TOKEN)
+                .set('Accept', 'application/json')
+                .query(params)
+                .end((err, res) => {
+                    if (err) fnReject(err);
+                    else {
+                        const parsed = JSON.parse(res.text);
+                        fnResolve(parsed);
+                    }
+                });
         });
-    });
-  }
+    }
 
 }
 
