957ab3ecea2bcaf170da065489aaa7b864c5a8aa
Eric Hulburd <eric@arbol.org>
2017-10-25 21:46:02 -0500

    small fixes to actions

--- build_production.sh
+++ build_production.sh
@@ -1,7 +1,8 @@
 rm -r ./build &&\
   mkdir -p ./build/production/assets/font-awesome &&\
   cp -rf ./server/assets/* ./build/production/assets/ &&\
-  cp ./server/views/index.ejs ./build/production/index.html &&\
-  cp -rf ./node_modules/font-awesome/**/* ./build/production/assets/font-awesome &&\
+  cp ./server/views/index.html ./build/production
+exit 1 # this doesn't work and must be run manually from command line.
+  cp -rf ./node_modules/font-awesome/ ./build/production/assets/font-awesome &&\
   mv ./build/production/assets/favicon.ico ./build/production/favicon.ico &&\
   npm run build_production
